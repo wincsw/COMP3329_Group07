a23773e76cb62fc05270116e362fb9d3bc812474
wincsw <winnie.chanszewing@gmail.com>
2023-04-21 16:21:56 +0800

    lock room in OnExitLevel script

--- Assets/Script/OnExitLevel.cs
+++ Assets/Script/OnExitLevel.cs
@@ -7,6 +7,9 @@ public class OnExitLevel : MonoBehaviour
 {
     public string sceneName;
     public bool isNextScene = true;
+    public bool lockRoom = false;
+    public string objNeed;
+    public Dialogue dialogue;
     [SerializeField]
     public SceneInfo sceneInfo;
 
@@ -16,8 +19,53 @@ public class OnExitLevel : MonoBehaviour
         if (SceneManager.GetActiveScene().name == "Corridor")
         {
             sceneInfo.lastPosition = GameObject.FindGameObjectWithTag("Player").transform.position;
+            switch (objNeed)
+            {
+                case "Soap":
+                    if (InventoryItem_Soap.collected)
+                    {
+                        lockRoom = true;
+                    }
+                    break;
+                case "ToiletPaper":
+                    if (InventoryItem_ToiletPaper.collected)
+                    {
+                        lockRoom = true;
+                    }
+                    break;
+                case "Key":
+                    if (InventoryItem_Key.collected)
+                    {
+                        lockRoom = true;
+                    }
+                    break;
+                case "Plunger":
+                    if (InventoryItem_Plunger.collected)
+                    {
+                        lockRoom = true;
+                    }
+                    break;
+
+            }
         }
-        Debug.Log("Entering " + sceneName);
-        SceneManager.LoadScene(sceneName);
+        else if (SceneManager.GetActiveScene().name == "DreamChamber")
+        {
+            lockRoom = false;
+        }
+
+        if (lockRoom == true)
+        {
+            Debug.Log("Already collected item from " + sceneName);
+            FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+        }
+        else
+        {
+            Debug.Log("Entering " + sceneName);
+            SceneManager.LoadScene(sceneName);
+
+        }
+
+
+
     }
 }
